b47cadd2a0527a924cd113128e1772751e917a43
Ali Churcher <churcher.ali@gmail.com>
2017-02-14 13:50:14 +0100

    Simple http server returns the given post request in upercase.

---
+++ httpUppercaser.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const map = require('through2-map');
+
+const server = http.createServer((request, response) => {
+    request.pipe(map((chunk) => chunk.toString().toUpperCase())).pipe(response);
+});
+server.listen(process.argv[2]);
